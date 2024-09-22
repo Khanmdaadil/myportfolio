c75260666a77272416405ad1dce073ce96826b47
Aadil <200030001@iitdh.ac.in>
2024-09-22 15:02:30 +0530

    fix

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -18,7 +18,7 @@ function Footer() {
           <div className="flex items-center gap-5">
             <Link
               target="_blank"
-              href="https://github.com/Khanmdaadil/myportfolio"
+              href="https://github.com/Khanmdaadil/myportfolio/"
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <IoStar />
